a8ccc5df3e1627264d0dc68676b2a14d25a7e877
vroenVen <42852953+vroenVen@users.noreply.github.com>
2026-02-12 00:58:48 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
   public static void main(String[] args){
     System.out.println("hello testing");
     String test = null;
+    test= "hello";
     System.out.println(test);
   }
   
